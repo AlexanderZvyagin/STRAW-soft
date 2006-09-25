7ea0513df0e8c54d81f6b3395ddb318bff0989cb
Alexander Zvyagin <zvyagin@lxplus018.cern.ch>
2006-09-26 01:43:40 +0200

    Terminal fancy output is added.

--- CS.py/CS/coral_lsf_jobs.py
+++ CS.py/CS/coral_lsf_jobs.py
@@ -1,6 +1,53 @@
 # @brief Submit CORAL jobs to LSF
 
-import os,sys,optparse,shutil
+import os,sys,optparse,shutil,re
+
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+
+RESET = '\033[0;0m'
+BOLD = '\033[1m'
+REVERSE = '\033[2m'
+
+BLACKBG = '\033[40m'
+REDBG = '\033[41m'
+GREENBG = '\033[42m'
+YELLOWBG = '\033[43m'
+BLUEBG = '\033[44m'
+MAGENTABG = '\033[45m'
+CYANBG = '\033[46m'
+WHITEBG = '\033[47m'
+
+def no_colors():
+    global BLACK,RED,GREEN,YELLOW,BLUE,MAGENTA,CYAN,WHITE,RESET,BOLD,REVERSE
+    global BLACKBG,REDBG,GREENBG,YELLOWBG,BLUEBG,MAGENTABG,CYANBG,WHITEBG
+
+    BLACK = ''
+    RED = ''
+    GREEN = ''
+    YELLOW = ''
+    BLUE = ''
+    MAGENTA = ''
+    CYAN = ''
+    WHITE = ''
+    RESET = ''
+    BOLD = ''
+    REVERSE = ''
+
+    BLACKBG = ''
+    REDBG = ''
+    GREENBG = ''
+    YELLOWBG = ''
+    BLUEBG = ''
+    MAGENTABG = ''
+    CYANBG = ''
+    WHITEBG = ''
 
 def main():
 
@@ -16,6 +63,12 @@ def main():
 
     parser.add_option('', '--submit',dest='submit',default=False,action='store_true',
                       help='Submit jobs at the end!')
+
+    parser.add_option('', '--rm',dest='rm',default=False,action='store_true',
+                      help='Remove output directory (if they exist)')
+
+    parser.add_option('', '--noterm',dest='noterm',default=False,action='store_true',
+                      help='Do not use fancy output (terminal properties).')
     
     (options, args) = parser.parse_args()
 
@@ -23,17 +76,28 @@ def main():
         parser.print_help()
         return 1
 
-    coral_dir = args[0]
-    coral_exe = args[1]
-    coral_opt = args[2]
+    coral_dir = os.path.abspath(args[0])
+    coral_exe = os.path.abspath(args[1])
+    coral_opt = os.path.abspath(args[2])
     run       = int(args[4])
     run_dir   = os.getcwd()+'/'+str(run)
-    output    = args[3]+'/'+str(run)
+    output    = os.path.abspath(args[3]+'/'+str(run))
+
+    try:
+        if options.noterm:
+            raise ''
+        import term
+        terminal = term.TerminalController()
+    except:
+        terminal = None
+        no_colors()
+
 
     # First we check for a 'CORAL' environment variable.
     # If it is not available, we set it and run the script again.
     if not os.environ.get('CORAL'):
-        print 'CORAL is not initialized. I will do this now.'
+        print RED+'CORAL is not initialized. I will do this now.'+RESET
+
         # We have to do (under bash):
         # 1) start a bash script which will:
         # 3) cd $CORAL
@@ -56,12 +120,12 @@ def main():
         bash_run = 'bash -c "%s"' % ' && '.join(commands)
         print '******************************'
         print '*** Executing the command: ***'
-        print bash_run
+        print BOLD+BLUE+bash_run+RESET
         print '******************************'
 
         return os.system(bash_run)
     else:
-        print 'CORAL path is: ',os.environ.get('CORAL')
+        print BOLD,RED,YELLOWBG,'CORAL path is: ',os.environ.get('CORAL'),RESET
 
     if os.path.abspath(os.environ.get('CORAL'))!=os.path.abspath(coral_dir):
         print 'Your CORAL variable is not the same as in the command line!'
@@ -76,7 +140,13 @@ def main():
         os.system(coral_exe)
         return 1
 
-    print 'Creating output directories...'
+    print GREEN,'Creating output directories...',RESET
+
+    if options.rm:
+        print RED,BOLD
+        os.system('rm -r %s > /dev/null 2>&1' % run_dir)
+        os.system('rfrm -r %s' % output)
+        print RESET
 
     # Create directory for temporary files
     os.mkdir(run_dir)
@@ -111,13 +181,28 @@ def main():
         extra = ''
 
     if os.system('cs lsf --queue=%s --coral=%s --output=%s %s cdr*.opt' % (queue,coral_exe,output,extra)):
-        print 'Failed to execute the jobs! Why?'
+        print RED,BOLD,'Failed to create the jobs list! Why?',RESET
         return 1
     
     if options.submit:
-        if os.system('./jobs.sh > jobs.log'):
-            print 'Something went wrong in the jobs submitting!'
-        n_jobs = int(os.popen('wc jobs.log').readline().split()[0])
+        n_jobs = 0
+        if terminal:
+            progress = term.ProgressBar(terminal, 'Submitting LSF jobs for run %d' % run)
+        for l in file('jobs.sh').readlines():
+            job = l.strip()
+            r = re.match('.*cdr(?P<cdr>\d+).*',job)
+            if not r:
+                name = 'Unknwon'
+            else:
+                name = r.group('cdr')
+            if terminal:
+                progress.update(float(n_jobs)/chunks, 'Chunk %s' % name)
+            n_jobs += 1
+            if os.system('echo %s > /dev/null' % job):
+                print RED,BOLD,'Something went wrong in the job submitting!',RESET
+                break
+        if terminal:
+            progress.update(float(n_jobs)/chunks, 'FINISHED')
         print 'It were submitted %d job(s) out of %d chunk(s).' % (n_jobs,chunks)
     else:
         print 'File "%s" is ready for an execution.' % os.path.abspath('jobs.sh')
