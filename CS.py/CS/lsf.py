b1f4f9daf30b96b04f8a6fac0ecd022b9dd52f4e
Alexander Zvyagin <zvyagin@lxplus060.cern.ch>
2006-09-27 11:40:24 +0200

    Copy option files to castor.

--- CS.py/CS/lsf.py
+++ CS.py/CS/lsf.py
@@ -1,7 +1,7 @@
 import re,os,sys,optparse
 
 def main():
-    parser = optparse.OptionParser(version='1.0.0')
+    parser = optparse.OptionParser(version='1.0.1')
     parser.description = 'Prepare LSF jobs to start.'
     parser.usage = 'cs %prog [options] <coral1.opt> [<coral2.opt> ...]\n'\
                    'Author: Alexander.Zvyagin@cern.ch'
@@ -41,7 +41,11 @@ def main():
     else:
         cp_command = 'cp'
 
-    for opt in args:
+    options.coral  = os.path.abspath(options.coral)
+    options.output = os.path.abspath(options.output)
+
+    for o in args:
+        opt = os.path.abspath(o)
         options.jobs_max -= 1
         if options.jobs_max<0:
             print 'Maximum number of jobs is reached. Stopping.'
@@ -53,17 +57,21 @@ def main():
         
         # Now the shell script is created.
         name,ext = os.path.splitext(opt)
-        script_sh = os.getcwd()+'/'+name+'.sh'
+        name = os.path.split(name)[1]
+        script_sh = os.path.abspath(name)+'.sh'
         script = file(script_sh,'w')
         os.system('chmod +x %s' % script_sh)
-        coral_run = '%s %s > /tmp/%s.log 2>&1' % (options.coral,os.path.abspath(opt),name)
+        coral_run = '%s %s > /tmp/%s.log 2>&1' % (options.coral,opt,name)
         script.write(coral_run + '\n\n')
 
         for f in [ ('mDST-','.root'), ('','.root'), ('','.gfile'), ('','.log')]:
             f_in  = '/tmp/%s%s%s' % (f[0],name,f[1])
-            f_out = '%s/%s%s%s' % (os.path.abspath(options.output),f[0],name,f[1])
-            script.write( '%s %s %s\n' % (cp_command,f_in,f_out))
+            f_out = '%s/%s%s%s' % (options.output,f[0],name,f[1])
+            script.write('%s %s %s\n' % (cp_command,f_in,f_out))
             script.write('rm -f %s\n' % f_in)
+
+        script.write('%s %s %s\n' % (cp_command,opt,options.output))
+        script.write('rm -f %s %s\n' % (script_sh,opt))
 
         if options.queue:
             all_cmds.write('bsub -q %s -oo lsf-%s.log ' % (options.queue,name) )
