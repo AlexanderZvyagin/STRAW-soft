7df7e2ad872b29882708a05f493b9d6d5ca54c67
Alexander ZVYAGIN <zvyagin@na58pc052.cern.ch>
2007-03-28 15:29:24 +0200

    Use "cs db".

--- T0/rough/run-prep.py
+++ T0/rough/run-prep.py
@@ -3,5 +3,5 @@ run  = 37059
 ddd  = '/afs/cern.ch/user/z/zvyagin/DDD.git/examples/how-to/ddd'
 opts = '--maps=/afs/cern.ch/compass/detector/maps/2004.xml'
 fout = file('run.sh','w')
-for f in os.popen('python /afs/cern.ch/user/z/zvyagin/cs.git/db.py --run=%d' % run):
+for f in os.popen('cs db --run=%d' % run):
     fout.write('%s --plugin=%s/straw_times.so %s %s\n\n' % (ddd,os.getcwd(),opts,f.strip()) )
