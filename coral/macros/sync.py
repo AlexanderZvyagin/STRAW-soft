06309cf4de6f4ad9c2e1d1ac0c7ba509b69749aa
Alexander ZVYAGIN <zvyagin@na58pc052.cern.ch>
2007-06-06 11:37:46 +0200

    Remove an old unneded file and add "xray.root" to the repository.

--- coral/macros/sync.py
+++
@@ -1,8 +0,0 @@
-import os
-
-repository = 'file://$HOME/w0/svn/Detectors'
-os.system('rm -fr Detectors.svn')
-#os.system('mv -f Detectors Detectors.old')
-os.system('svn co %s Detectors.svn' % repository)
-#os.system('find Detectors.svn -name .svn | xargs rm -rf')
-os.system('cp Detectors.svn/src/* Detectors/src')
